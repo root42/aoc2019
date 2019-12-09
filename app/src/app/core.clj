1b074b48ede67994c9408f9d4984a06d9ee388c0
Schmitz, Arne <arne.schmitz@nuance.com>
2019-12-09 16:03:24 +0100

    day 1, task 2

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -6,15 +6,33 @@
     (map #(Integer. %) (clojure.string/split-lines input))) 
   )
 
+(defn fuel-req
+  [mass]
+  (int (- (Math/floor (/ mass 3) ) 2) )
+  )
+
 (defn fuel-reqs
   [modules]
-  (map #(int (- (Math/floor (/ % 3) ) 2))  modules)
+  (map #(fuel-req %) modules )
+  )
+
+(defn rec-fuel-req
+  [mass]
+  (if (<= mass 0)
+    0
+    (+ mass (rec-fuel-req (fuel-req mass))))
+  )
+
+(defn rec-fuel-reqs
+  [modules]
+  (reduce + (map #(rec-fuel-req (fuel-req %)) modules))
   )
 
 (defn -main
   "Advent of Code 2019."
   [& args]
   (let [input (read-input "1.txt")]
-    (println "Sum of fuel requirements: " (reduce + (fuel-reqs input)))
+    (println "1.1 Sum of fuel requirements: " (reduce + (fuel-reqs input)))
+    (println "1.2 Sum of fuel requirements: " (rec-fuel-reqs input))
     )
   )
